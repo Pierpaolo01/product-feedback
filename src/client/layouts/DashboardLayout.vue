0ad34a242ac1c5f478aad81bffc806697274aef4
Pierpaolo Pascarella <Pierpaolo.pascarella@gmail.com>
2022-11-23 20:54:31 +0100

    Task: mobile main layout

--- src/client/layouts/DashboardLayout.vue
+++ src/client/layouts/DashboardLayout.vue
@@ -1,10 +1,38 @@
 <script setup lang="ts">
 import TheHeaderComponent from "@client/components/TheHeaderComponent.vue";
+import hamburgerMenu from "@client/assets/icons/hamburger-menu.svg"
+import cross from "@client/assets/icons/cross.svg"
+import {reactive} from "vue";
+
+const state = reactive<{
+  toggleNav: boolean;
+}>({
+  toggleNav: false
+})
 </script>
 
 <template>
-  <div class="flex flex-col">
-    <TheHeaderComponent />
+  <div class="w-full fixed top-0">
+    <div class="flex flex-col md:hidden">
+      <div class="gradient h-16 flex justify-between items-center px-6">
+        <h1 class="text-center text-white font-bold">
+          product feedback
+        </h1>
+        <img
+            :src="!state.toggleNav ? hamburgerMenu : cross"
+            alt="hamburger_menu"
+            @click="state.toggleNav = !state.toggleNav"
+        >
+      </div>
+      <TheHeaderComponent />
+    </div>
+    <aside
+        v-if="state.toggleNav"
+        class="p-6 bg-white fixed top-16 right-0 h-full"
+    >
+      <div class="w-[250px]">dsfs</div>
+      dfs
+    </aside>
   </div>
   <div class="hidden md:flex flex-col">
     <aside>
@@ -18,3 +46,9 @@ import TheHeaderComponent from "@client/components/TheHeaderComponent.vue";
     </main>
   </div>
 </template>
+
+<style scoped>
+.gradient {
+  background: radial-gradient(128.88% 128.88% at 103.9% -10.39%, #E84D70 0%, #A337F6 53.09%, #28A7ED 100%);
+}
+</style>
